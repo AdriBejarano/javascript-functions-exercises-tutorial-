abf783497df7f9159a672809c1f4e96604c37682
AdriBejarano <adriana_bt@hotmail.com>
2021-01-29 23:18:21 +0000

    ejercicio7

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,3 +1,16 @@
+const rapid = (str)=>{
+    let newWords = [];
+    let vowels = ['a','e','i','o','u'];
+    for (let i = 0; i < str.length; i++) {
+        if(vowels.includes(str[i])){
+            console.log(str[i]);
+        }else{
+            newWords.push(str[i].toUpperCase());
+        }
+    }
+    return newWords.join('');
+}
+
 
    // From this line up Do not change code below
    let str = "John";
